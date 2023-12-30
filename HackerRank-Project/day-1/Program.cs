6043288df9b024f76e476de8b6718ab096e29f7c
Enes <20224753011@ogr.akdeniz.edu.tr>
2023-12-31 01:19:28 +0300

    Add files via upload

---
+++ HackerRank-Project/day-1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace day_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        String inputString; 
+        inputString = Console.ReadLine(); 
+        Console.WriteLine("Hello, World.");
+        Console.Write(inputString);
+    }
+}
